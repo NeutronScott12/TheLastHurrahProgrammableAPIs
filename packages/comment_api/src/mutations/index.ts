665c1ae1d84afed7072e46a35faf5573ec93de7c
scott <scottberry91@gmail.com>
2022-07-25 14:42:53 +0100

    exported more types and functions so common can use the new hooks

--- packages/comment_api/src/mutations/index.ts
+++ packages/comment_api/src/mutations/index.ts
@@ -1,2 +1,21 @@
 export * from './CommentMutations'
 export * from './hooks'
+export type {
+    CreateCommentMutationHookResult,
+    EditThreadCommentMutationHookResult,
+    DeleteThreadCommentMutationHookResult,
+    CreateReplyCommentMutationHookResult,
+    UpVoteCommentMutationHookResult,
+    DownVoteCommentMutationHookResult,
+    CreateCommentReportMutationHookResult,
+} from '../generated/graphql'
+export {
+    useCreateCommentMutation,
+    useEditThreadCommentMutation,
+    useDeleteThreadCommentMutation,
+    useCreateReplyCommentMutation,
+    useUpVoteCommentMutation,
+    useDownVoteCommentMutation,
+    useCreateCommentReportMutation,
+} from '../generated/graphql'
+export * from './helpers/functions'
--- packages/comment_api/src/queries/index.ts
+++ packages/comment_api/src/queries/index.ts
@@ -1,2 +1,6 @@
 export * from './CommmentQueries'
 export * from './hooks'
+
+export type { FetchCommentByThreadIdQueryHookResult } from '../generated/graphql'
+
+export { useFetchCommentByThreadIdQuery } from '../generated/graphql'
